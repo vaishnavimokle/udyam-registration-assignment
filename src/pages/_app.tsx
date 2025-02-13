1d827664f609230a6afc6c8276c2c05d9bc4331f
Shubh Parmar <shubh.parmar@decentro.tech>
2025-02-13 15:44:06 +0530

    initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { createTheme, Button, MantineProvider } from "@mantine/core";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+const theme = createTheme({
+  fontFamily: "Montserrat, sans-serif",
+  components: {
+    Button: Button.extend({
+      defaultProps: {
+        radius: "xl",
+        variant: "gradient",
+        gradient: { from: "#096ef2", to: "#0092ff", deg: 90 },
+      },
+    }),
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
